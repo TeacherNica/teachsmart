68d2ee4bec44e679e9d88bc45fed2052b78b87d5
TeacherNica <nica081598@gmail.com>
2026-04-13 05:41:28 +0700

    Add class buttons to student cards

--- addlogin.js
+++ addlogin.js
@@ -1,8 +1,8 @@
 ﻿const fs = require('fs');
 let h = fs.readFileSync('index.html', 'utf8');
 
-const oldBtn = "onclick=\"openModal('report-modal')\">📝 Report</button>\n        ${s.classes<=2?";
-const newBtn = "onclick=\"openModal('report-modal')\">📝 Report</button>\n        <button class=\"s-btn\" style=\"background:#DCFCE7;color:#15803D;\" onclick=\"addClass(${s.id})\">➕ Add</button>\n        <button class=\"s-btn\" style=\"background:#FEE2E2;color:#B91C1C;\" onclick=\"deductClass(${s.id})\">➖ Deduct</button>\n        <button class=\"s-btn\" style=\"background:#FEF3C7;color:#B45309;\" onclick=\"deleteStudent(${s.id})\">🗑️ Delete</button>\n        ${s.classes<=2?";
+const oldBtn = "Report</button>\r\n        ${s.classes<=2?";
+const newBtn = "Report</button>\r\n        <button class=\"s-btn\" style=\"background:#DCFCE7;color:#15803D;\" onclick=\"addClass(${s.id})\">➕ Add</button>\r\n        <button class=\"s-btn\" style=\"background:#FEE2E2;color:#B91C1C;\" onclick=\"deductClass(${s.id})\">➖ Deduct</button>\r\n        <button class=\"s-btn\" style=\"background:#FEF3C7;color:#B45309;\" onclick=\"deleteStudent(${s.id})\">🗑️ Delete</button>\r\n        ${s.classes<=2?";
 
 h = h.replace(oldBtn, newBtn);
 fs.writeFileSync('index.html', h, 'utf8');
