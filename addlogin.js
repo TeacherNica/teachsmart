007318a40fc8a4b1683da4fb1bd590adef4243e4
TeacherNica <nica081598@gmail.com>
2026-04-13 08:12:48 +0700

    Fix dashboard schedule hardcode

--- addlogin.js
+++ addlogin.js
@@ -1,13 +1,12 @@
 ﻿const fs = require('fs');
 let h = fs.readFileSync('index.html', 'utf8');
 
-// Fix getTodaySlots day index
-// getDay(): 0=Sun,1=Mon,2=Tue,3=Wed,4=Thu,5=Fri,6=Sat
-// WEEKLY_SCHEDULE: 0=Mon,1=Tue,2=Wed,3=Thu,4=Fri,5=Sat,6=Sun
-const oldHelper = 'function getTodaySlots(){\n  const dayIdx=new Date().getDay()===0?6:new Date().getDay()-1;';
-const newHelper = 'function getTodaySlots(){\n  const d=new Date().getDay();const dayIdx=d===0?6:d-1;';
+const oldSched = "  const sched=[\r\n    {time:'5:30 PM',s:students[0],status:'done'},\r\n    {time:'6:30 PM',s:students[1],status:'now'},\r\n    {time:'7:30 PM',s:students[2],status:'upcoming'},\r\n    {time:'8:30 PM',s:students[3],status:'upcoming'},\r\n  ];";
 
-h = h.replace(oldHelper, newHelper);
+const count = h.split(oldSched).length - 1;
+console.log('found:', count);
+
+h = h.replaceAll(oldSched, '  const sched=getTodaySlots();');
 
 fs.writeFileSync('index.html', h, 'utf8');
-console.log('fixed:', h.includes('const d=new Date().getDay()'));
+console.log('replaced:', !h.includes("students[0],status:'done'"));
