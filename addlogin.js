7d37520ebe533930518c7a0d9d72aa0a47b62278
TeacherNica <nica081598@gmail.com>
2026-04-13 19:00:05 +0700

    Fix schedule: remove Rainy Friday, add Coco-2 and Shily Saturday

--- addlogin.js
+++ addlogin.js
@@ -1,9 +1,18 @@
 ﻿const fs = require('fs');
 let h = fs.readFileSync('index.html', 'utf8');
 
-const oldLine = "  today.innerHTML=sched.map(({time,s,status})=>{";
-const newLine = "  today.innerHTML=sched.map(({time,s,status})=>{\r\n    if(!isToday) status='upcoming';";
+// Fix 1: Remove Rainy from Friday (index 4)
+h = h.replace(
+  "    {time:'7:30 PM', student:'Rainy', duration:25},\n    {time:'8:00 PM', student:'Owen', duration:25},\n  ],\n  5:",
+  "    {time:'8:00 PM', student:'Owen', duration:25},\n  ],\n  5:"
+);
 
-h = h.replace(oldLine, newLine);
+// Fix 2: Add Coco-2 at 11:00 AM Saturday and Shily at 11:30 AM Saturday
+h = h.replace(
+  "    {time:'10:00 AM', student:'Seah', duration:50},\n    {time:'11:00 AM', student:'Shily', duration:25},",
+  "    {time:'10:00 AM', student:'Seah', duration:50},\n    {time:'11:00 AM', student:'Coco-2', duration:25},\n    {time:'11:30 AM', student:'Shily', duration:25},"
+);
+
 fs.writeFileSync('index.html', h, 'utf8');
-console.log('fixed:', h.includes("if(!isToday) status='upcoming'"));
+console.log('friday fixed:', !h.includes("7:30 PM', student:'Rainy', duration:25},\n    {time:'8:00 PM', student:'Owen'"));
+console.log('saturday fixed:', h.includes("11:00 AM', student:'Coco-2'") && h.includes("11:30 AM', student:'Shily'"));
