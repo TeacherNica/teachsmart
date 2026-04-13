221e7ad914401e278f945596aa57cb8923732ea7
TeacherNica <nica081598@gmail.com>
2026-04-13 14:43:44 +0700

    Remove duplicate getTodaySlots

--- addlogin.js
+++ addlogin.js
@@ -1,8 +1,15 @@
 ﻿const fs = require('fs');
 let h = fs.readFileSync('index.html', 'utf8');
 
-// Remove the setTimeout that resets dashboard
-h = h.replace('setTimeout(function(){renderDashboard();},200);', '');
+// Find both occurrences
+const first = h.indexOf('function getTodaySlots');
+const second = h.indexOf('function getTodaySlots', first + 10);
 
+console.log('first at:', first, 'second at:', second);
+
+// Remove the second one
+const secondEnd = h.indexOf('\nfunction ', second + 10);
+h = h.substring(0, second) + h.substring(secondEnd);
+
+console.log('count after:', h.split('function getTodaySlots').length - 1);
 fs.writeFileSync('index.html', h, 'utf8');
-console.log('removed:', !h.includes('setTimeout(function(){renderDashboard'));
