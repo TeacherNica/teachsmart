65e75ef3af64edeedcb598f3bdd8410874858fd7
TeacherNica <nica081598@gmail.com>
2026-04-13 19:49:09 +0700

    Revert "Add markAttendance function"
    This reverts commit f204111223def307ca8a848b6a8b3b46351a7281.

--- attend.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let h = fs.readFileSync('index.html', 'utf8');
-h = h.replace('function isUpcoming', markFn + '\nfunction isUpcoming');
-fs.writeFileSync('index.html', h, 'utf8');
-console.log('done:', h.includes('markAttendance'));
