13a4bb1dec8a4ee93551a98d58959cc5a090d393
TeacherNica <nica081598@gmail.com>
2026-04-13 19:07:21 +0700

    Fix Saturday schedule Coco-2 and Shily

--- addlogin.js
+++ addlogin.js
@@ -1,17 +1,9 @@
 ﻿const fs = require('fs');
 let h = fs.readFileSync('index.html', 'utf8');
 
-const oldSat = `  5: [
-    {time:'10:00 AM', student:'Seah', duration:50},
-    {time:'11:00 AM', student:'Shily', duration:25},
-    {time:'12:00 PM', student:'Owen', duration:50},`;
-
-const newSat = `  5: [
-    {time:'10:00 AM', student:'Seah', duration:50},
-    {time:'11:00 AM', student:'Coco-2', duration:25},
-    {time:'11:30 AM', student:'Shily', duration:25},
-    {time:'12:00 PM', student:'Owen', duration:50},`;
+const oldSat = "5: [\r\n    {time:'10:00 AM', student:'Seah', duration:50},\r\n    {time:'11:00 AM', student:'Shily', duration:25},\r\n    {time:'12:00 PM', student:'Owen',";
+const newSat = "5: [\r\n    {time:'10:00 AM', student:'Seah', duration:50},\r\n    {time:'11:00 AM', student:'Coco-2', duration:25},\r\n    {time:'11:30 AM', student:'Shily', duration:25},\r\n    {time:'12:00 PM', student:'Owen',";
 
 h = h.replace(oldSat, newSat);
 fs.writeFileSync('index.html', h, 'utf8');
-console.log('fixed:', h.includes("'11:00 AM', student:'Coco-2'"));
+console.log('fixed:', h.includes("'Coco-2', duration:25"));
